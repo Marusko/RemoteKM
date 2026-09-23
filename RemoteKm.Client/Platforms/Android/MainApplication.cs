c5a702aca331b0bf6f148edfcf2b21ee03f33f5c
Marusko <matussusky@gmail.com>
2026-09-23 17:58:14 +0200

    Added handler to log java exceptions

--- RemoteKm.Client/Platforms/Android/MainApplication.cs
+++ RemoteKm.Client/Platforms/Android/MainApplication.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Runtime;
+using RemoteKm.Client.Services;
 
 namespace RemoteKm.Client;
 
@@ -9,7 +10,26 @@ public class MainApplication : MauiApplication
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 		: base(handle, ownership)
 	{
+		// A crash on a Java thread (camera, barcode decoders) kills the process without ever
+		// reaching the .NET handlers in App, so it would leave no trace in the log.
+		Java.Lang.Thread.DefaultUncaughtExceptionHandler =
+			new JavaCrashLogger(Java.Lang.Thread.DefaultUncaughtExceptionHandler);
 	}
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	/// <summary>Logs a fatal Java exception, then hands it to the previous handler as before.</summary>
+	private sealed class JavaCrashLogger : Java.Lang.Object, Java.Lang.Thread.IUncaughtExceptionHandler
+	{
+		private readonly Java.Lang.Thread.IUncaughtExceptionHandler? _next;
+
+		public JavaCrashLogger(Java.Lang.Thread.IUncaughtExceptionHandler? next) => _next = next;
+
+		public void UncaughtException(Java.Lang.Thread thread, Java.Lang.Throwable ex)
+		{
+			AppLog.Error($"Fatal Java exception on thread '{thread.Name}':{Environment.NewLine}" +
+				Android.Util.Log.GetStackTraceString(ex));
+			_next?.UncaughtException(thread, ex);
+		}
+	}
 }
